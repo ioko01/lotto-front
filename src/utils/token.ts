7417bd38312aa62786f8c8c2c6d2d02bd40e9030
unknown <ioko.peezaza@gmail.com>
2024-01-20 20:44:47 +0700

    commit

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,5 +1,5 @@
 export const getToken = () => {
   // Retrieve the token from local storage or your state management library
-  const token = localStorage.getItem(import.meta.env.VITE_OPS_COOKIE_NAME);
+  const token = sessionStorage.getItem(import.meta.env.VITE_OPS_COOKIE_NAME);
   return token;
 };
