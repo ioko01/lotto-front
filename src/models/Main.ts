6d21b4d51e8a1fe71efbb6c57b79afae239d6b18
ioko01 <68391123+ioko01@users.noreply.github.com>
2024-01-31 09:00:58 +0700

    Update Main.ts

--- src/models/Main.ts
+++ src/models/Main.ts
@@ -3,12 +3,12 @@ import { IUserDoc } from "./User"
 export interface IInitialState {
     created_at?: Date
     updated_at?: Date
-    // admin_create_id?: IUserDoc
-    // agent_create_id?: IUserDoc
-    // manager_create_id?: IUserDoc
-    // user_create_id?: IUserDoc
-    admin_create_id?: string
-    agent_create_id?: string
-    manager_create_id?: string
-    user_create_id?: string
+    admin_create_id?: IUserDoc
+    agent_create_id?: IUserDoc
+    manager_create_id?: IUserDoc
+    user_create_id?: IUserDoc
+    // admin_create_id?: string
+    // agent_create_id?: string
+    // manager_create_id?: string
+    // user_create_id?: string
 }
